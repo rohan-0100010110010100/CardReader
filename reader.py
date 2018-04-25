a6c3ddc43c409f2bc1f7d05ba0b1c9f34a7b5c90
Rohan Verma <rohan40594@gmail.com>
2018-04-25 21:11:58 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -9,7 +9,6 @@ image = cv2.resize(image,(512,341))
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # ---------------------------------------------------------- #
 
-## TODO: Define the geometric tranform function
 ## This function take in an image and returns a 
 ## geometrically transformed image
 def geo_tx(image):
@@ -24,14 +23,13 @@ def geo_tx(image):
         
          ])
     
-    ## TODO: Define the four destination coordinates    
-    ## Tip: These points should define a 400x200px rectangle
+
     warped_pts = np.float32([[0,0],[image.shape[1],0],[0,image.shape[0]],[image.shape[1],image.shape[0]]])
     
-    ## TODO: Compute the perspective transform, M
+    #Compute the perspective transform, M
     M = cv2.getPerspectiveTransform(source_pts,warped_pts)
     
-    ## TODO: Using M, create a warped image named `warped`
+    ## Using M, create a warped image named `warped`
     warped =  cv2.warpPerspective(image,M,(image.shape[1], image.shape[0]))
 
     return warped
